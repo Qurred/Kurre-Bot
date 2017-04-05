b633b85e7c7678b248606a2716bc3dfc28754050
Juho Kuusinen <qurred@gmail.com>
2017-04-05 10:23:14 +0300

    Git Repo start

---
+++ kurre-bot.js
@@ -0,0 +1,73 @@
+const discord = require('discord.js');
+var client = new discord.Client();
+//scripts
+var basics = require('./scripts/basics.js')(client);
+//Jsons
+var config = require('./data/config.json');
+var comments = require('./addons/data/comments.json');
+var members = require('./addons/data/members.json');
+
+var komennot = [
+  '!ping - palauttaa pong, enimmäkseen testaamista varten',
+  '!bilis - luo uuden biljardipelin (2v2)',
+  '!kiima - komento jonka niki toivoi, en tiedä miksi',
+  '!pelaajat - palauttaa pelaajat',
+  '!uusiPelaaja [nimi] - lisää uuden pelaajan',
+  '!help - auttaa casuaaleja',
+  '!komennot - mitä luulet tämän olevan?'
+];
+
+client.on('ready', () => {
+  console.log('Currently running version: ' + config.version);
+  console.log('Bot\'s home server is ' + config.home_server.name);
+  console.log(`Logged in as ${client.user.username}!`);
+  var guilds = client.guilds.array();
+  for(var i = 0; i < guilds.length;i++){
+    if(!guilds[guilds[i].id]){
+      var guild_id = guilds[i].id;
+      members.guilds[guild_id] = {};
+    }
+      var memArr = guilds[i].members.array();
+      var guild_members = {};
+      for(var j = 0; j < memArr.length; j++){
+         var member_datas = {};
+         member_datas['name'] =  memArr[j].displayName;
+         member_datas['last_online'] = "";
+         member_datas['osu_id'] = "";
+         member_datas['battle_name'] = "";
+         guild_members[memArr[j].id] = member_datas
+        }
+        members.guilds[guild_id]=guild_members;
+    }
+    console.log('Settings done');
+});
+
+client.on('message', msg => {
+  var message = msg.content;
+  message = message.split(" ");
+  if(message[0] === '!komennot'){
+    var res = "Botti tuntee tällä hetkellä seuraavat komennot:";
+    for(var i = 0; i < komennot.length; i++){
+          res +="```" + komennot[i] + "```";
+    }
+    msg.channel.sendMessage(res);
+  }else if(message[0] === '!roskiin'){
+    msg.channel.sendMessage('Hei hei :sunglasses: ');
+    setTimeout(function(){process.exit(0)}, 1500);
+  }else if(message[0] === '!lainaus'){
+    if(msg.guild){
+      var commenter = msg.guild.members.get(comments.comments[0].commenter.id);
+      var res = new discord.RichEmbed()
+      .setTitle('Lainaus')
+      .setAuthor(commenter.displayName, commenter.user.avatarURL)
+      .setColor(0x00AE86)
+      .setDescription(comments.comments[0].comment)
+      .setFooter('Lisännyt ' + comments.comments[0].adder.username + ' ' + comments.comments[0].adder.date);
+      msg.channel.sendEmbed(res);
+    }
+  }else if(message[0] === '!datat' && msg.author.id === config.owner_id){
+    msg.channel.sendMessage(JSON.stringify(members.guilds));
+  }
+});
+
+client.login(config.token);
---
+++ scripts/basics.js
@@ -0,0 +1,48 @@
+var bilis = require('../data/players.json');
+
+module.exports = function(client){
+  client.on('message', msg => {
+    var message = msg.content;
+    message = message.split(" ");
+    if (message[0] === '!ping') {
+      msg.reply('Pong!');
+    }
+    else if(message[0] === '!bilis'){
+      var tmp = "Joukkueet: ";
+      var lista = [];
+      for(var i = 0; i < bilis.pelaajat.length; i++){
+        lista.push(bilis.pelaajat[i]);
+      }
+      for(var i = 0; i < 4; i++){
+        var index = Math.floor((Math.random() * lista.length));
+        tmp += lista[index];
+        lista.splice(index, 1);
+        if(i == 1){
+          tmp+= " vs ";
+        }
+      }
+      msg.channel.sendMessage(tmp);
+    }
+    else if(message[0] === '!kiima'){
+        msg.reply('Mee roskiin!');
+    }
+    else if(message[0] === '!pelaajat'){
+      var res = "Pelaajia ovat: "
+      for(var i = 0; i < bilis.pelaajat.length-1; i++){
+        res += bilis.pelaajat[i] + ", ";
+      }
+      res += bilis.pelaajat[bilis.pelaajat.length-1];
+      msg.channel.sendMessage(res);
+    }
+    else if(message[0] === '!uusiPelaaja'){
+      if(message[1]){
+        bilis.pelaajat.push(message[1]);
+      }else{
+        msg.reply('Määrittele lisättävä pelaaja: \'!uusiPelaaja [pelaajan nimi]\'');
+      }
+    }
+    else if(message[0] === '!help'){
+      msg.channel.sendMessage('https://www.youtube.com/watch?v=6B7-tMh9zyI');
+    }
+  });
+};
